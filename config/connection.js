2199991f8ef41f3f334a94660d63d8ed0c891b93
Ray Thomas <rthomas5678@gmail.com>
2024-01-08 19:23:16 -0700

    added connection update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString = mongodb+srv://rthomas5678:GCULopes$17@cluster0.lbav8zq.mongodb.net/?retryWrites=true&w=majority;
+mongoose.connect('mongodb+srv://rthomas5678:GCULopes$17@cluster0.lbav8zq.mongodb.net/?retryWrites=true&w=majority');
 
-connect(connectionString);
-
-module.exports = connection;
+module.exports = mongoose.connection;
